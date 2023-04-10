86abc860bb407394ceb2e84a4fae09de509502a6
Ing-ed <emidovichi@gmail.com>
2023-04-10 17:55:37 -0300

    ajuste de estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
 import { useState, useEffect } from 'react'
+import "./App.css"
+
+function Form(){
+  let [getDolars,setDolars] = useState(1);
+  let [getValD, setValD] = useState(0)
+  let [getValP, setValP] = useState(0)
+
+  function onchange(evt){
+    setValD(evt.target.value)
+    console.log(getDolars)
+    console.log(+getValD * +getDolars)
+  }
+  function onchange2(evt){
+    setValP(evt.target.value)
+    console.log(evt.target.value)
+  }
+  useEffect(() =>{
+    fetch("https://www.dolarsi.com/api/api.php?type=dolar").then(res => res.json())
+    .then((res) => setDolars(res[0].casa.venta.split(",")[0]))
+    // .then((res) => console.log(res[0].casa.venta))
+    .catch((err) => console.log(err))
+  },[])
+  return(
+    <section>
+      <div>
+        <input onChange = {onchange} type='number' placeholder='u$s'/>
+        <h3>a$r {+getValD * +getDolars}</h3>
+      </div>
+      <div>
+        <input onChange = {onchange2} type = 'number' placeholder='a$r'/>
+        <h3>u$s {getValP / +getDolars}</h3>
+      </div>
+    </section>
+  )
+}
 
 function App() {
-  let [getDolarOfi,setDolarOfi] = useState([]);
   let jjjj = "Hola"
-  useEffect(() =>{
-    fetch("https://www.dolarsi.com/api/api.php?type=dolar").then(res => res.json()).then((res) => setDolarOfi(res)).catch((err) => console.log(err))
-  },[])
-  console.log(getDolarOfi[0],"dolarOfi")
-  jjjj = getDolarOfi[0]["casa"]["venta"].split(",")[0]
-  console.log(jjjj,typeof jjjj)
-  return (
-    <h1>{jjjj}</h1>
-  )
+  // jjjj = getDolarOfi[0]["casa"]["venta"].split(",")[0]
+    return(
+      <Form/>
+    )
 }
 
 export default App
